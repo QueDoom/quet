ee7ec8014355d6830404ef7129e8e4ba43970911
QueDoom <quedoom@totl.se>
2026-09-20 22:39:32 +0200

    Fixed it

--- src/main/java/net/quedoom/quet/QueTDataGenerator.java
+++ src/main/java/net/quedoom/quet/QueTDataGenerator.java
@@ -2,7 +2,6 @@ package net.quedoom.quet;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.quedoom.quet.datagen.*;
-import net.quedoom.quet.datagen.test.TagProvider;
 
 public class QueTDataGenerator extends QueTDataGeneratorEntrypoint {
 	@Override
